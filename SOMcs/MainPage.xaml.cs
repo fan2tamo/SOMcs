cec66b3314ce39860d1ae7a44a3382e03eb72042
tfukushima1 <tfukushima1@necp.co.jp>
2018-12-25 19:12:53 +0900

    プロジェクト ファイルを追加します。

---
+++ SOMcs/MainPage.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace SOMcs
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SOM som;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string map_size = cbx_map_size.SelectedValue.ToString();
+            //int size = Convert.ToInt32(map_size);
+           som = new SOM();
+            canvas.Height = som.Height * 32;
+            canvas.Width = som.Width * 32;
+            double[] r = new double[3];
+            r[0] = 1.0;
+            r[1] = 0;
+            r[2] = 0;
+            SOM.Node rNode = new SOM.Node(3, vector: r, label:"R");
+
+            double[] g = new double[3];
+            g[0] = 0;
+            g[1] = 1.0;
+            g[2] = 0;
+            SOM.Node gNode = new SOM.Node(3, vector: g, label: "G");
+
+            double[] b = new double[3];
+            b[0] = 0;
+            b[1] = 0;
+            b[2] = 1.0;
+            SOM.Node bNode = new SOM.Node(3, vector: b, label: "B");
+
+            double[] rg = new double[3];
+            rg[0] = 1.0;
+            rg[1] = 1.0;
+            rg[2] = 0.0;
+            SOM.Node rgNode = new SOM.Node(3, vector: rg, label: "RG");
+
+            double[] rb = new double[3];
+            rb[0] = 1.0;
+            rb[1] = 0.0;
+            rb[2] = 1.0;
+            SOM.Node rbNode = new SOM.Node(3, vector: rb, label: "RB");
+
+            double[] gb = new double[3];
+            gb[0] = 0.0;
+            gb[1] = 1.0;
+            gb[2] = 1.0;
+            SOM.Node gbNode = new SOM.Node(3, vector: gb, label: "GB");
+
+            List<SOM.Node> nodes = new List<SOM.Node>();
+            nodes.Add(rNode);
+            nodes.Add(gNode);
+            nodes.Add(bNode);
+            nodes.Add(rgNode);
+            nodes.Add(rbNode);
+            nodes.Add(gbNode);
+            som.Learning(nodes);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            byte r, g, b;
+
+            int radius = 10;
+            for (int h = 0; h < som.Height; h++)
+            {
+                for (int w = 0; w < som.Width; w++)
+                {
+                    if ((h + w) % 2 == 0)
+                    {
+                        r = (byte)(som.Nodes[h, w].WeightVector[0] * 255);
+                        g = (byte)(som.Nodes[h, w].WeightVector[1] * 255);
+                        b = (byte)(som.Nodes[h, w].WeightVector[2] * 255);
+                        Polygon p = CreateHex(radius, Color.FromArgb(255, r, g, b));
+                        p.Tag = som.Nodes[h, w];
+                        canvas.Children.Add(p);
+                        p.PointerPressed += HexClick;
+                        if (h % 2 == 0)
+                        {
+                            //Canvas.SetLeft(p, 3 * radius * w);
+                        }
+                        else
+                        {
+                            //Canvas.SetLeft(p, radius * 3 * w + 1.5 * radius);
+                        }
+                        Canvas.SetLeft(p, 1.5 * radius * w);
+                        Canvas.SetTop(p, radius * h);
+                    }
+                }
+            }
+        }
+
+        private async Task UpdateCanvas(SOM.Node[,] nodes, int height, int width)
+        {
+            byte r, g, b;
+
+            int radius = 30;
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                for (int h = 0; h < height; h++)
+                {
+                    for (int w = 0; w < width; w++)
+                    {
+                        if ((h + w) % 2 == 0)
+                        {
+                            r = (byte)(nodes[h, w].WeightVector[0] * 255);
+                            g = (byte)(nodes[h, w].WeightVector[1] * 255);
+                            b = (byte)(nodes[h, w].WeightVector[2] * 255);
+                            Polygon p = CreateHex(radius, Color.FromArgb(255, r, g, b));
+                            p.Tag = nodes[h, w].Coordinate;
+                            canvas.Children.Add(p);
+                            p.PointerPressed += HexClick;
+                            if (h % 2 == 0)
+                            {
+                                //Canvas.SetLeft(p, 3 * radius * w);
+                            }
+                            else
+                            {
+                                //Canvas.SetLeft(p, radius * 3 * w + 1.5 * radius);
+                            }
+                            Canvas.SetLeft(p, 1.5 * radius * w);
+                            Canvas.SetTop(p, radius * h);
+                        }
+                    }
+                }
+            });
+        }
+
+        private Polygon CreateHex(int radius, Color color)
+        {
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(new Point(0.5 * radius, Math.Sqrt(3) / 2 * radius));
+            myPointCollection.Add(new Point(-0.5 * radius, Math.Sqrt(3) / 2 * radius ));
+            myPointCollection.Add(new Point(-1 * radius, 0 * radius ));
+            myPointCollection.Add(new Point(-0.5 * radius, -Math.Sqrt(3) / 2 * radius));
+            myPointCollection.Add(new Point(0.5 * radius, -Math.Sqrt(3) / 2 * radius ));
+            myPointCollection.Add(new Point(1 * radius, 0 * radius));
+            Polygon myPolygon = new Polygon();
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush(color);
+            SolidColorBrush brush = new SolidColorBrush(Colors.White);
+            myPolygon.Points = myPointCollection;
+            myPolygon.Fill = mySolidColorBrush;
+            myPolygon.Width = radius * 2;
+            myPolygon.Height = radius * 2;
+            myPolygon.Stretch = Stretch.Fill;
+            myPolygon.Stroke = brush;
+            myPolygon.StrokeThickness = 2;            
+            return myPolygon;
+        }
+
+        private void HexClick(object sender, PointerRoutedEventArgs e)
+        {
+            Polygon p = sender as Polygon;
+            SOM.Node buf = p.Tag as SOM.Node;
+            Debug.WriteLine($"{buf.Coordinate}, label = {buf.Label}, vector = {buf.WeightVector[0]}, {buf.WeightVector[1]}, {buf.WeightVector[2]}");
+        }
+    }
+}
---
+++ SOMcs/SOM.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOMcs
+{
+
+
+    public class SOM
+    {
+        public class Node
+        {
+
+            static Random rand;
+
+            /// <summary>
+            /// 重みベクトル
+            /// </summary>
+            public double[] WeightVector { get; private set; }
+
+            /// <summary>
+            /// Map上の座標
+            /// </summary>
+            public Point Coordinate { get; private set; }
+
+            /// <summary>
+            /// コンストラクタ.
+            /// </summary>
+            /// <param name="dimensionNumber">重みベクトルの次元数</param>
+            public Node(int dimensionNumber, Point coordinate = new Point(), string label = null, double[] vector = null, int seed = 0)
+            {
+                if (Node.rand == null)
+                {
+                    Node.rand = new Random(seed);
+                }
+
+                Coordinate = coordinate;
+                WeightVector = new double[dimensionNumber];
+                if (vector == null)
+                {
+                    for (int i = 0; i < dimensionNumber; i++)
+                    {
+                        WeightVector[i] = rand.NextDouble();
+                    }
+                }
+                else
+                {
+                    vector.CopyTo(WeightVector, 0);
+                }
+
+                Label = label;
+            }
+
+            public string Label { get; set; }
+
+            public void Update(double[] target, double ratio, string label)
+            {
+                for (int i = 0; i < WeightVector.Length; i++)
+                {
+                    WeightVector[i] += ratio  * (target[i] - WeightVector[i]) ;
+                }
+
+                Label = label;
+            }
+        }
+
+        public int Height{ get; private set; }
+
+        public int Width { get; private set;}
+
+        /// <summary>
+        /// 初期半径率
+        /// </summary>
+        public double InitialRadiusRatio { get; private set; }
+
+        /// <summary>
+        /// 初期学習率
+        /// </summary>
+        public double InitialLearningRatio { get; private set; }
+
+        /// <summary>
+        /// 学習回数
+        /// </summary>
+        public int LearningTime{ get; private set; }
+
+
+        /// <summary>
+        /// 出力層のノード
+        /// </summary>
+        public SOM.Node[,] Nodes { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="height">出力層の縦方向のサイズ</param>
+        /// <param name="width">出力層の横方向のサイズ</param>
+        /// <param name="initialLearningRatio">初期学習係数</param>
+        /// <param name="initRadiusRatio">初期半径率</param>
+        /// <param name="learningTime">学習回数</param>
+        /// <param name="dimensionNumber">入力ベクトルの次元数</param>
+        public SOM(int height = 80, int width = 80, double initialLearningRatio = 0.4, double initRadiusRatio = 0.5, int learningTime = 500 , int dimensionNumber = 3)
+        {
+            Height = height;
+            Width = width;
+            Nodes = new Node[Height, Width];
+            InitialLearningRatio = initialLearningRatio;
+            InitialRadiusRatio = initRadiusRatio;
+            LearningTime = learningTime;
+
+            for (int h = 0; h < Height; h++)
+            {
+                for (int w = 0; w < Width; w++)
+                {
+                    if ((h + w) % 2 == 0)
+                    {
+                        Nodes[h, w] = new Node(dimensionNumber, new Point(w, h));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <returns></returns>
+        public bool Learning(List<SOM.Node> nodes)
+        {
+            bool ret = false;
+            Point winnerPoint;
+            
+            for(int t = 1; t <= LearningTime; t++)
+            {
+                foreach (Node node in nodes)
+                {
+                    // 勝者ノードの探索
+                    winnerPoint = SearchWinnerNode(node.WeightVector);
+
+                    // 近傍領域の更新
+                    Update(node, winnerPoint, t);                
+                }
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 入力ベクトルに一番距離が近いノードの座標を返す
+        /// </summary>
+        /// <param name="vector">入力ベクトル</param>
+        /// <returns></returns>
+        private Point SearchWinnerNode(double[] vector)
+        {
+            double dist = 0xeffffffff;
+            Point winnerPoint = new Point(0, 0);
+            double buf = 0;
+
+            foreach (Node node in Nodes)
+            {
+                if (node != null)
+                {
+                    buf = CalcDistance(vector, node);
+                    if (buf <= dist)
+                    {
+                        dist = buf;
+                        winnerPoint = node.Coordinate;
+                    }
+                }
+            }
+
+            return winnerPoint;
+        }
+
+        /// <summary>
+        /// 入力ベクトルとノード間の距離を計算する。
+        /// </summary>
+        /// <param name="vector">入力ベクトル</param>
+        /// <param name="node">ノード</param>
+        /// <returns></returns>
+        private double CalcDistance(double[] vector, Node node)
+        {
+            double dist = 0;
+
+            for(int i = 0; i < node.WeightVector.Length; i++)
+            {
+                dist += Math.Abs(vector[i] - node.WeightVector[i]);
+            }
+
+            return dist;
+
+        }
+
+        /// <summary>
+        /// 勝者ノードを含む近傍ノードの更新
+        /// </summary>
+        /// <param name="target">入力ベクトル</param>
+        /// <param name="winnerPoint">勝者ノードの座標</param>
+        /// <param name="t">学習回数</param>
+        /// <returns></returns>
+        private bool Update(Node target, Point winnerPoint, int t)
+        {
+            bool ret = false;
+            int hammingDist = 0;
+            double learningRatio = CalcLearningRatio(t);
+            double ratio;
+            int radius = CalcLearningRadius(t);
+            int h_n = winnerPoint.Y - radius;
+            int h_p = winnerPoint.Y + radius;
+            int w_n = winnerPoint.X - radius;
+            int w_p = winnerPoint.X + radius;
+
+            if(h_n < 0)
+            {
+                h_n = 0;
+            }
+
+            if(h_p >= Height)
+            {
+                h_p = Height - 1;
+            }
+
+            if (w_n < 0)
+            {
+                w_n = 0;
+            }
+
+            if (w_p >= Width)
+            {
+                w_p = Width- 1;
+            }
+
+            for (int h = h_n; h <= h_p; h++)
+            {
+                for (int w = w_n; w <= w_p; w++)
+                {
+                    hammingDist = Math.Abs(h - winnerPoint.Y) + Math.Abs(w - winnerPoint.X);
+                    if (hammingDist <= radius)
+                    {
+                        if ((h + w) % 2 == 0)
+                        {
+                            ratio = CalcRatio(hammingDist, learningRatio);
+                            Nodes[h, w].Update(target.WeightVector, ratio, target.Label);
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 学習回数に応じた近傍領域
+        /// </summary>
+        /// <param name="t">学習回数</param>
+        /// <returns></returns>
+        private int CalcLearningRadius(int t)
+        {
+            int ret =  (int)(InitialRadiusRatio * (Height < Width ? Height : Width) * (1 -   (double)t / LearningTime));
+            return ret;
+        }
+
+        /// <summary>
+        ///学習回数に応じた 学習係数を計算する
+        /// </summary>
+        /// <param name="t">現在の学習回数</param>
+        /// <returns></returns>
+        private double CalcLearningRatio(int t)
+        {
+            double ret = InitialLearningRatio * (1.0 -  (double)t / LearningTime);
+            return ret;
+        }
+
+        /// <summary>
+        /// 勝者ノードとの距離に応じた学習係数を計算する
+        /// </summary>
+        /// <param name="hammingDist">勝者ノードとの距離</param>
+        /// <param name="learningRatio">学習に応じた学習係数</param>
+        /// <returns></returns>
+        private double CalcRatio(int hammingDist, double learningRatio)
+        {
+            return learningRatio / (hammingDist  + 1.0);
+        }
+    }
+}
